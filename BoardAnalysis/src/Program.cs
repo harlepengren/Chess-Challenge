0edab578c5c6702cafd70ef619afdf3b70a7ff6a
kkoehler4070 <koehler.62@gmail.com>
2023-08-15 07:43:28 -0400

    Update Program.cs
    Added ability to read a json file.

--- BoardAnalysis/src/Program.cs
+++ BoardAnalysis/src/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace BoardAnalysis.Application
 {
 	public class Program
 	{
+		public static void ReadFile(string filename)
+		{
+			List<GameInfo> source = new List<GameInfo>();
+
+			using (StreamReader r = new StreamReader(filename))
+			{
+				string json = r.ReadToEnd();
+				source = JsonSerializer.Deserialize<List<GameInfo>>(json);
+			}
+
+			Evaluate evaluate = new Evaluate();
+
+			foreach(GameInfo currentGame in source)
+			{
+				evaluate.LoadFEN(currentGame.FEN);
+				ScoreStruct currentScore = evaluate.EvaluatePosition();
+				Console.WriteLine(currentScore);
+			}
+		}
+
         public static void Main()
         {
-			string FEN;
+			Console.WriteLine("Enter a json file: ");
+			string jsonFile = Console.ReadLine();
+
+			ReadFile(jsonFile);
+
+			/*string FEN;
 			ScoreStruct score;
 			List<string> inputFENs = new List<string>();
 			
@@ -36,9 +62,17 @@ namespace BoardAnalysis.Application
 			Console.WriteLine("Center Score: " + score.centerScore);
 			Console.WriteLine("Piece Score: " + score.pieceScore);
 			Console.WriteLine("Rook Score: " + score.rooksScore);
-			Console.WriteLine("Checkmate Score: " + score.checkmateScore);
+			Console.WriteLine("Checkmate Score: " + score.checkmateScore);*/
 		}
 
+	}
+
+	public class GameInfo
+	{
+		public string FEN { get; set; }
+		public int totalMoves { get; set; }
+		public int currentMove { get; set; }
+		public string winner { get; set; }
 	}
 }
 
