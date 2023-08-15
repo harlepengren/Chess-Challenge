c9d3b9cdd8e44d6edd118a83c0d199fa4d530a24
kkoehler4070 <koehler.62@gmail.com>
2023-08-15 05:39:12 -0400

    Update MoveExport.py
    Added empty method and pseudocode for processing all games in a directory.

--- BoardAnalysis/src/PGNReader/MoveExport.py
+++ BoardAnalysis/src/PGNReader/MoveExport.py
@@ -4,7 +4,8 @@ import random
 
 class MoveExporter:
     def ProcessGame(self, filename):
-        currentGame = Replay(filename)
+        currentGame = Replay()
+        currentGame.ReadFile(filename)
 
         numMoves = currentGame.MoveCount()
         randomMove = random.randint(0,numMoves-1)
@@ -15,11 +16,21 @@ class MoveExporter:
         # Determine the current player, this assumes we start with white
         # a better option would be to implement tracking in the board
         player = "w" if (randomMove % 2) == 0 else "b"
-        gameDict["FEN"] = currentGame.board.Export("FEN",player)
+        gameDict["FEN"] = currentGame.board.ExportFEN(player)
         gameDict["totalMoves"] = numMoves
         gameDict["move"] = randomMove
 
         if(currentGame.winner != None):
             gameDict["winner"] = currentGame.winner
 
-        print(gameDict)
+        return gameDict
+
+    def LoadGames(self, directory):
+    '''Loads all games in a directory, processes them, and exports a json file.'''
+        # Check whether the directory is valid
+        # for each file in directory
+        # process game and add game to list
+
+        # save the list as a JSON file
+
+        pass
